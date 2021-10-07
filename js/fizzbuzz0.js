fb31b1e9501333a5fb116fb0638724198d16f9f5
LufkinJulie <69699210+LufkinJulie@users.noreply.github.com>
2021-10-07 12:20:24 -0400

    Add files via upload

---
+++ js/fizzbuzz0.js
@@ -0,0 +1,39 @@
+function getNames() 
+{
+	let fName = document.getElementById('fName').value;
+	let lName = document.getElementById('lName').value;
+	let mInitial = document.getElementById('mInitial').value;
+	
+	if (mInitial) {
+		document.getElementById('welcome').textContent = 'Welcome to Old Dog Coding, ' + fName +  ' ' + mInitial + '. ' lName + '!';
+	} else {
+		document.getElementById('welcome').textContent = 'Welcome to Old Dog Coding, ' + fName + ' ' + lName + '!';
+	}
+	
+	let numCount = prompt(~How high can you count ${fName}?~);
+	answer(numCount);
+}
+function answer(numCount)
+{
+	for(i = 0; i <= numCount; i++) {
+		var evenOdd;
+		
+		if(i % 2 === 0) {
+			evenOdd = "This number is even";
+		} else {
+			evenOdd = "This number is odd";
+		}
+		// list under ul
+		var newEl = document.createElement('li');
+		
+		// text output
+		var newList = document.createTextNode(i + 'Old Dog Coding -' + evenOdd);
+		
+		// append info
+		newEl.appendChild(newList);
+		
+		// where to place on page
+		var place = document.getElementsByTagName('ul')[0];
+		place,appendChild(newE
+}
+		
